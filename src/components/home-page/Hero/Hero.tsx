93adb428d8c720e257d1078093c1fe82f65f1eff
Chris Ware <c.p.ware@hotmail.com>
2025-03-20 18:56:43 -0700

    c

--- src/components/home-page/Hero/Hero.tsx
+++ src/components/home-page/Hero/Hero.tsx
@@ -1,19 +1,19 @@
-import LayoutWrapper from "@/components/shared/LayoutWrapper";
+// import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import styles from "./Hero.module.css";
 import RotatingText from "../RotatingText/RotatingText";
 
 export default function Hero() {
   return (
     <section className={styles.container}>
-      <LayoutWrapper>
-        <div className={styles.content}>
-          <div className={styles.top}>
-            <RotatingText text='Live mas • Live mas • Live mas • Live mas • Live mas • Live mas •' />
-            <span className={styles.span}>French non-alcoholic spirits</span>
-            <h1 className={styles.heading}>the aperitif reinvented</h1>
-          </div>
+      {/* <LayoutWrapper> */}
+      <div className={styles.content}>
+        <div className={styles.top}>
+          <RotatingText text='Live mas • Live mas • Live mas • Live mas • Live mas • Live mas •' />
+          <span className={styles.span}>French non-alcoholic spirits</span>
+          <h1 className={styles.heading}>the aperitif reinvented</h1>
         </div>
-      </LayoutWrapper>
+      </div>
+      {/* </LayoutWrapper> */}
     </section>
   );
 }
