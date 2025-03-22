8d7d31712146daafd887b2f36fe0c0ac1dc16020
Chris Ware <c.p.ware@hotmail.com>
2025-03-21 20:58:12 -0700

    PP Section done

--- src/components/home-page/ProductPreviewSection/ProductPreviewSection.tsx
+++ src/components/home-page/ProductPreviewSection/ProductPreviewSection.tsx
@@ -5,35 +5,29 @@ import LayoutWrapper from "@/components/shared/LayoutWrapper";
 import Image from "next/image";
 import Img1 from "../../../../public/images/about.jpg";
 import Img2 from "../../../../public/images/aboutiii.jpg";
-import Img3 from "../../../../public/images/contactii.jpg";
-import Img4 from "../../../../public/images/contactiii.jpeg";
+import Img3 from "../../../../public/images/contactiii.jpeg";
 import { useState } from "react";
 
 const data = [
   {
     id: 3,
     animation: Img1,
-    title: "Maximize your revenue potential",
-    desc: "Drive more sales with optimized user experiences, fast load times, and conversion-focused design that turns visitors into loyal customers.",
+    title: "Our Award Winning Tonics",
+    desc: "Best soft drink innovation 2025",
   },
   {
     id: 1,
     animation: Img2,
-    title: "Optimized for mobile-first shoppers (70% of e-commerce traffic)",
-    desc: "We build websites that help your online store reach new heights, combining speed, scalability, and style to make your business stand out.",
+    title: "Alchol Free Flammarion",
+    desc: "New: Book from the founder of JNPR",
   },
   {
     id: 2,
     animation: Img3,
-    title: "PCI-compliant payment gateways integrated seamlessly",
-    desc: "Simplify your workflow with seamless order processing, real-time updates, and automated confirmations to keep your customers coming back.",
+    title: "RHHM No. 1",
+    desc: "Discover our new alcohol-free spirit",
   },
-  {
-    id: 4,
-    animation: Img4,
-    title: "SEO structures built to rank product pages, not just blogs",
-    desc: "Easily manage your store from anywhere with user-friendly tools that give you control over products, sales, and analytics—all in one place.",
-  },
+
 ];
 
 export default function ProductPreviewSection() {
