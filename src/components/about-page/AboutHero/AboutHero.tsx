0a0bde7190152149772a60e3906c5841ce83e90f
Chris Ware <c.p.ware@hotmail.com>
2025-03-22 22:53:13 -0700

    About hero done

--- src/components/about-page/AboutHero/AboutHero.tsx
+++ src/components/about-page/AboutHero/AboutHero.tsx
@@ -1,21 +1,7 @@
 import RotatingText from "@/components/home-page/RotatingText/RotatingText";
 import styles from "./AboutHero.module.css";
 import LayoutWrapper from "@/components/shared/LayoutWrapper";
-
-const data = [
-  {
-    id: 1,
-    title: "The Story",
-  },
-  {
-    id: 2,
-    title: "The Honey Project",
-  },
-  {
-    id: 3,
-    title: "The Art of Distilation",
-  },
-];
+import Lol from "../../../../public/icons/lol.svg";
 
 export default function AboutHero() {
   return (
@@ -28,35 +14,39 @@ export default function AboutHero() {
             iconColor='blueIcon'
           />
           <div className={styles.dataBox}>
-            {data.map((x) => (
-              <span key={x.id} className={styles.span}>
-                {x.title}
-              </span>
-            ))}
+            {/* {data.map((x) => ( */}
+            <span className={styles.span}>
+              {/* {x.title} */}
+              How did we get our start?
+            </span>
+            {/* ))} */}
           </div>
-          <h1 className={styles.heading}>Our Story</h1>
-          <LayoutWrapper>
-            <p className={styles.copy}>
-              During a trip to the United States, Valérie, the founder,
-              experienced a much more varied range of drinks, with subtle
-              flavors, natural ingredients, and properties that make them true
-              everyday allies. The first thoughts about JNPR then germinated.
-              <br />
-              <br />
-              Through her research, she discovered that during the 17th century,
-              distilled juniper berry and other plants and spices such as
-              coriander seed were highly sought after, renowned in particular
-              for their medicinal properties.
-              <br />
-              <br />
-              She met Flavio Angiolillo, a renowned bartender, and together,
-              they reflected on the best way to perpetuate this tradition by
-              setting themselves an additional challenge: maintaining excellent
-              mixology properties, even without alcohol. After months of
-              reading, research, testing, and meetings, JNPR was born.
-            </p>
-          </LayoutWrapper>
+          <h1 className={styles.heading}>Funny Story</h1>
+          <div className={styles.iconContainer}>
+            <Lol className={styles.icon} />
+          </div>
         </div>
+        <LayoutWrapper>
+          <p className={styles.copy}>
+            During a trip to the United States, Valérie, the founder,
+            experienced a much more varied range of drinks, with subtle flavors,
+            natural ingredients, and properties that make them true everyday
+            allies. The first thoughts about JNPR then germinated.
+            <br />
+            <br />
+            Through her research, she discovered that during the 17th century,
+            distilled juniper berry and other plants and spices such as
+            coriander seed were highly sought after, renowned in particular for
+            their medicinal properties.
+            <br />
+            <br />
+            She met Flavio Angiolillo, a renowned bartender, and together, they
+            reflected on the best way to perpetuate this tradition by setting
+            themselves an additional challenge: maintaining excellent mixology
+            properties, even without alcohol. After months of reading, research,
+            testing, and meetings, JNPR was born.
+          </p>
+        </LayoutWrapper>
       </div>
     </section>
   );
