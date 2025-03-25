ed9ed295cf828b66c77e58fb9c1c77e948a222e7
Chris Ware <c.p.ware@hotmail.com>
2025-03-25 16:37:07 -0700

    nav announcement update

--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -1,10 +1,12 @@
 import styles from "./Nav.module.css";
+import NavAnnouncement from "./NavAnnouncement/NavAnnouncement";
 import NavLinks from "./NavLinks/NavLinks";
 import NavbarIcons from "./NavbarIcons/NavbarIcons";
 
 export function Nav() {
   return (
     <header className={styles.header}>
+      <NavAnnouncement />
       <div className={styles.navBar}>
         <NavLinks />
         <NavbarIcons />
